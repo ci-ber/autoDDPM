e4c917107df09c36186a490ea9e4a1685780f2fa
Cosmin Bercea <cosmin.bercea@tum.de>
2023-12-06 02:05:30 +0100

    refactor

--- projects/autoddpm/DownstreamEvaluator.py
+++ projects/autoddpm/DownstreamEvaluator.py
@@ -225,7 +225,6 @@ class PDownstreamEvaluator(DownstreamEvaluator):
                 else:
                     data0 = data[0]
                 x = data0.to(self.device)
-                print(f'min: {torch.min(x)}, max: {torch.max(x)} mean: {torch.mean(x)}')
                 masks = data[1].to(self.device)
                 masks[masks>0] = 1
 
